153d52e36dc79272ccb839700623f31ef3911b92
ClaraY05 <clara.w.yee@gmail.com>
2025-06-29 13:56:22 -0700

    made switch statements for each logic expression

--- D2_IntroToArduinoIDE/Switch.ino
+++ D2_IntroToArduinoIDE/Switch.ino
@@ -4,37 +4,47 @@
   Switch.ino
 */
 // set up constants
-const int pin_red = 13;     // set up pin values for easier reference in code
-const int pin_green = 12;
-uint32_t timeNow = 0;       // create an unsigned long to hold millis time
+const int pin_a = 13;     // set up pin values for easier reference in code
+const int pin_b = 12;
+const int logic = 0; // 0 = a && b, 1 = a || b, 2 = !(a||b), 3 = a && !b
+const int pin_out = 10;
+const bool will_light = false;
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(pin_red, OUTPUT);
-  pinMode(pin_green, OUTPUT);
+  // initialize digital pin LED_BUILTIN as an input pullup.
+  pinMode(pin_a, INPUT_PULLUP);
+  pinMode(pin_b, INPUT_PULLUP);
+  pinMode(pin_out, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-    timeNow = millis();
+  switch(logic){
+    // set logic
+    case 0: // lights the LED if button1 AND button2 are pressed
+      will_light = ((!digitalRead(pin_a))&&(!digitalRead(pin_b)));
+      break;
+    case 1: // lights the LED if button1 OR button2 are pressed
+      will_light = ((!digitalRead(pin_a))||(!digitalRead(pin_b)));
+      break;
+    case 2: // lights the LED if NEITHER button1 NOR button2 are pressed
+      will_light = (!((!digitalRead(pin_a))||(!digitalRead(pin_b))));
+      break;
+    case 3: //lights the LED if button1 is pressed AND button2 is NOT pressed
+      will_light = ((!digitalRead(pin_a))&&(digitalRead(pin_b)));
+      break;
+    default:
+      will_light = false;
+      break;
+  }
 
-    // code for RED LED (holds on every 750 ms)
-    if(timeNow%750 == 0){ // keep on for 1/2 of period
-         digitalWrite(pin_red, HIGH);  // turn the LED off (LOW is the voltage level)
+  // set pin mode
+    if(will_light){
+      digitalWrite(pin_out,HIGH);
     }
-    else if(timeNow%(750*2) == 0){ // turn off for other 1/2
-        digitalWrite(pin_red, LOW);  // turn the LED on (HIGH is the voltage level)
-
+    else{
+      digitalWrite(pin_out,LOW);
     }
-
-    // code for GREEN LED (holds on every 350 ms)
-    if(timeNow%350 == 0){
-         digitalWrite(pin_green, HIGH);
-    }
-    else if(timeNow%(350*2) == 0){
-        digitalWrite(pin_green, LOW);  
-    }
-  
 }
 
